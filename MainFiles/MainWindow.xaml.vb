28b217ba597b1d8dbbcebb74c4dfe2eca5a2801d
TWSFTS_07007 <119282947+SuoHouDaiShi@users.noreply.github.com>
2023-10-28 16:27:43 +0800

    Add files via upload

---
+++ MainFiles/MainWindow.xaml.vb
@@ -0,0 +1,11 @@
+﻿Class MainWindow
+    Private Sub HelpDocs(sender As Object, e As RoutedEventArgs)
+        System.Diagnostics.Process.Start("explorer.exe", "https://docs.qq.com/doc/DSWt4d1ZObHNud0xR")
+    End Sub
+    Private Sub SendMail(sender As Object, e As RoutedEventArgs)
+        System.Diagnostics.Process.Start("explorer.exe", "mailto:ArtisticCraftOffical@proton.me")
+    End Sub
+    Private Sub CrashHelp(sender As Object, e As RoutedEventArgs)
+        System.Diagnostics.Process.Start("explorer.exe", "https://www.bilibili.com/video/BV1xw41117KD")
+    End Sub
+End Class
